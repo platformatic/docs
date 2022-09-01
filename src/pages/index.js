230eb0ae11bb823ddd02139af8a6060201df61ec
Matteo Collina <hello@matteocollina.com>
2022-09-01 17:01:24 +0200

    Add homepage
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,6 +26,83 @@ function HomepageHeader() {
   );
 }
 
+function VideoEmbed() {
+  return (
+    <section className={styles.video}>
+      <div className="container">
+        <div class="rows">
+          <div class="col col--12">
+            <h1>Checkout the Platformatic DB basic features</h1>
+          </div>
+          <div class="col col--6 col--offset-3">
+            <iframe src="https://www.loom.com/embed/3a2052674be249ae95bb2b407cf0efac" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+            </iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+const quotesData = [{
+  name: "James Snell",
+  title: "Node.js Technical Steering Committee",
+  quote: "Quote from James",
+  avatar: "https://avatars.githubusercontent.com/u/439929?s=460&v=4"
+}, {
+  name: "Feross Aboukhadijeh",
+  title: "Founder & CEO, Socket.dev",
+  quote: "Quote from Feross",
+  avatar: "https://avatars.githubusercontent.com/u/121766?s=460&v=4"
+}, {
+  name: "Charlie Robbins",
+  title: "",
+  quote: "Quote from Charlie",
+  avatar: "https://avatars.githubusercontent.com/u/4624?s=460&v=4"
+}, {
+  name: "David Mark Clements",
+  title: "",
+  quote: "Quote from Dave",
+  avatar: "https://avatars.githubusercontent.com/u/1190716?s=460&v=4"
+}]
+
+function Quotes() {
+  return (
+    <section className={styles.quotes}>
+      <div class="container">
+        <div class="row">
+          <div class="col col--12">
+            <h1>Quotes</h1>
+          </div>
+
+          {quotesData.map((quote, index) => ( 
+          <div class="col col--3">
+            <div class="card">
+              <div class="card__header">
+                <div class="avatar">
+                  <img
+                    class="avatar__photo"
+                    src={quote.avatar} />
+                  <div class="avatar__intro">
+                    <div class="avatar__name">{quote.name}</div>
+                    <small class="avatar__subtitle">
+                      {quote.title}
+                    </small>
+                  </div>
+                </div>
+              </div>
+              <div class="card__body">
+                <p> {quote.quote} </p>
+              </div>
+            </div>
+          </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -34,7 +111,8 @@ export default function Home() {
       description="The documentation of all Open Source libraries that are part of Platformatic.">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <VideoEmbed />
+        <Quotes />  
       </main>
     </Layout>
   );
