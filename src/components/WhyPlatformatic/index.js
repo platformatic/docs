3653734254bae6a73fe2deb6f1021f4b3b6dd56a
Matteo Collina <hello@matteocollina.com>
2022-09-22 10:23:03 +0200

    Adjust text and descriptions
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- src/components/WhyPlatformatic/index.js
+++ src/components/WhyPlatformatic/index.js
@@ -14,7 +14,7 @@ export default function WhyPlatformatic() {
     <section class="margin-top--lg">
       <div class="container">
         <div className="row">
-          <div className="col col--6 {styles.why}">
+          <div className="col col--8 {styles.why}">
             <h1>Why Platformatic?</h1>
             <p>
               Platformatic enables developers to efficiently develop and run APIs at scale.
@@ -29,19 +29,19 @@ export default function WhyPlatformatic() {
             <p>
               Our goal is to make API development simple: we aim is to remove all friction from the day-to-day
               of backend developers. Platformatic is a series of Open Source tools to build APIs.
+              Check out our announcement <a href="https://www.loom.com/share/6a1715285d3d4479841240beaf175275">video</a>.
             </p> 
           </div>
-          <div className="col col--6">
-            <div className={styles.why} > 
-              <iframe src="https://www.loom.com/embed/6a1715285d3d4479841240beaf175275" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            </div>
+          <div className="col col--4">
+            <img src="img/plt-logo.svg" className={styles.light} />  
+            <img src="img/plt-logo-inverted.svg" className={styles.dark} />  
           </div>
         </div>
         <div className="row margin-top--lg">
-          <div className="col col--6">
+          <div className="col col--5">
             <img src="/img/platformatic-db-architecture.png" alt="Platformatic DB Architecture" />
           </div>
-          <div className="col col--6">  
+          <div className="col col--6 col--offset-1">  
             <h1>Our first tool: Plaformatic DB</h1>
             <p>
               The first friction we are solving is the tiresome mapping of databases table to APIs that
@@ -51,11 +51,9 @@ export default function WhyPlatformatic() {
               it via <a href="https://nodejs.org">Node.js</a> and
               <a href="https://www.fastify.io">Fastify</a> plugins.
             </p>
-          </div>
-        </div>
-        <div className="row margin-top--lg">
-          <div className={styles.features} class="col col--6">
-            <h1>Platformatic DB basic features</h1>
+            <p>
+              Checkout Platformatic DB basic features:
+            </p>
             <ul>
               <li>Automatic OpenAPI/REST API generation</li>
               <li>Automatic GraphQL API generation</li>
@@ -65,7 +63,9 @@ export default function WhyPlatformatic() {
               <li>Customizable via Node.js and Fastify plugins</li>
             </ul>
           </div>
-          <div class="col col--6">
+        </div>
+        <div className="row margin-top--lg">
+          <div class="col col--6 col--offset-3">
             <div className={styles.video}>
               <iframe src="https://www.loom.com/embed/3a2052674be249ae95bb2b407cf0efac" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
               </iframe>
