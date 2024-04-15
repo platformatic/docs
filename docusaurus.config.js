7256a7b177ffbbb60cc376c45ce9122382419e6c
Fortune Ikechi <ikechifortune@gmail.com>
2024-04-15 14:40:14 +0100

    hot fixes for docs nav || Signed-off-by: Fortune Ikechi <ikechifortune@gmail.com>
    Signed-off-by: Fortune Ikechi <ikechifortune@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,20 +80,10 @@ const config = {
             label: 'Get Started',
           },
           {
-            to: 'docs/category/reference',
-            position: 'left',
-            label: 'Reference',
-          },
-          {
-            to: 'docs/category/guides',
+            to: 'docs/guides/movie-quotes-app-tutorial',
             position: 'left',
             label: 'Guides',
           },
-          // {
-          //   to: 'docs/category/platformatic-cloud',
-          //   position: 'left',
-          //   label: 'Platformatic Cloud',
-          // },
           { href: 'https://blog.platformatic.dev', label: 'Blog', position: 'left' },
           { href: 'https://www.platformatic.dev', label: 'About', position: 'left' },
           {
@@ -119,12 +109,9 @@ const config = {
               },
               {
                 label: 'Guides',
-                to: 'docs/category/guides',
+                to: 'docs/guides/movie-quotes-app-tutorial',
               },
-              {
-                label: 'Reference',
-                to: 'docs/category/reference',
-              },
+              
             ],
           },
           {
