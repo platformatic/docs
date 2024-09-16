90308b6aceebfb479d6001c764a2c77a976b7222
Fortune Ikechi <ikechifortune@gmail.com>
2024-09-16 15:27:13 +0100

    fixed markdown styling bug (#159)
    Signed-off-by: Fortune Ikechi <ikechifortune@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,24 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 module.exports = {
-  darkMode: ['selector', '[data-theme="dark"]'],
+  darkMode: ["selector", '[data-theme="dark"]'],
   content: [
-    './src/**/*.{js,jsx,ts,tsx,mdx}',
-    './pages/**/*.{js,jsx,ts,tsx,mdx}',
-    './components/**/*.{js,jsx,ts,tsx,mdx}'
+    "./src/**/*.{js,jsx,ts,tsx,mdx}",
+    "./pages/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
   ],
+  corePlugins: { preflight: false },
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
       boxShadow: {
-        'custom-black': '0 4px 20px rgba(0, 0, 0, 0.15)',
-        'cta-custom-black': '1px 4px 8px rgba(0, 0, 0, 0.15)',
-        'custom-white': '1px 4px 8px rgba(255, 255, 255, 0.15)'
-      }
-    }
+        "custom-black": "0 4px 20px rgba(0, 0, 0, 0.15)",
+        "cta-custom-black": "1px 4px 8px rgba(0, 0, 0, 0.15)",
+        "custom-white": "1px 4px 8px rgba(255, 255, 255, 0.15)",
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
