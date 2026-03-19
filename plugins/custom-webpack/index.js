0c54f959413084a45195b9dc2fa0d969fe2511f1
Paolo Insogna <paolo@cowtech.it>
2026-03-19 12:39:35 +0100

    fix: Fixed search. (#202)
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- plugins/custom-webpack/index.js
+++ plugins/custom-webpack/index.js
@@ -1,15 +1,22 @@
 const webpack = require('webpack')
 const dotenv = require('dotenv')
+const path = require('path')
 
 dotenv.config()
 
 module.exports = function customWebpackPlugin () {
   return {
     name: 'custom-webpack-plugin',
-    configureWebpack (config, isServer, utils) {
+    configureWebpack () {
       return {
         resolve: {
+          fullySpecified: false,
+          alias: {
+            '@orama/react-components': path.resolve(process.cwd(), 'node_modules', '@orama', 'react-components', 'dist', 'index.mjs'),
+            'process/browser': path.resolve(process.cwd(), 'node_modules', 'process', 'browser.js')
+          },
           fallback: {
+            process: require.resolve('process/browser'),
             path: require.resolve('path-browserify'),
             os: require.resolve('os-browserify/browser'),
             crypto: require.resolve('crypto-browserify')
